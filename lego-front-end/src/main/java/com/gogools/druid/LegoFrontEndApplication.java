642308958e32aad71696fad619b77ecaaad9f3ef
dpulido <dpulido@10.153.20.134>
2017-12-12 19:17:12 -0600

    First commit Dec12

---
+++ lego-front-end/src/main/java/com/gogools/druid/LegoFrontEndApplication.java
@@ -0,0 +1,12 @@
+package com.gogools.druid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegoFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegoFrontEndApplication.class, args);
+	}
+}
