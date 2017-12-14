d706240feeee82c47f3b59b2ba1be6a23814a55b
dpulido <dpulido@10.152.201.237>
2017-12-13 19:05:08 -0600

    project creation Dec13 - RestoRoomReports

---
+++ r3/src/main/java/com/gogools/r3/R3Application.java
@@ -0,0 +1,12 @@
+package com.gogools.r3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(R3Application.class, args);
+	}
+}
---
+++ r3/src/test/java/com/gogools/r3/R3ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gogools.r3;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class R3ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
