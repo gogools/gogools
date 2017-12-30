52ab78b2d6d6d15cec07fc326fcdf43b7f9aa861
Dan <Dan@Dazgal>
2017-12-30 01:32:05 -0600

    spring reactive programming book

--- learning-spring-boot/src/main/java/com/greglturnquist/learningspringboot/Chapter.java
+++ learning-spring-boot/src/main/java/com/greglturnquist/learningspringboot/Chapter.java
@@ -4,8 +4,10 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Data
-@Document
+@Data 		// annotation from Lombok generates getters, setters, a toString()
+			// method, an equals() method, a hashCode() method, and a constructor for all
+			// required (that is, final) fields
+@Document 	// annotation flags this class as suitable for storing in a MongoDB data store
 public class Chapter {
 	
 	@Id
